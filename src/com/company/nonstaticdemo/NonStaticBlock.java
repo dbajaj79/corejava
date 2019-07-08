11470b69b6cb414dec5a32148dd30602fb20f0b6
Deepak <dkbajajmnk@gmail.com>
2019-07-08 12:51:24 +0530

    Non Static Blocks in Java

---
+++ src/com/company/nonstaticdemo/NonStaticBlock.java
@@ -0,0 +1,16 @@
+package com.company.nonstaticdemo;
+
+public class NonStaticBlock {
+
+    public static void main(String[] args) {
+        SimpleNonStaticBlock simpleNonStaticBlock = new SimpleNonStaticBlock();
+        SimpleNonStaticBlock simpleNonStaticBlock1 = new SimpleNonStaticBlock();
+        SimpleNonStaticBlock simpleNonStaticBlock2 = new SimpleNonStaticBlock();
+        SimpleNonStaticBlock simpleNonStaticBlock3 = new SimpleNonStaticBlock();
+        SimpleNonStaticBlock simpleNonStaticBlock4 = new SimpleNonStaticBlock();
+        SimpleNonStaticBlock simpleNonStaticBlock5 = new SimpleNonStaticBlock();
+        SimpleNonStaticBlock simpleNonStaticBlock6 = new SimpleNonStaticBlock();
+        SimpleNonStaticBlock simpleNonStaticBlock7 = new SimpleNonStaticBlock();
+
+    }
+}
---
+++ src/com/company/nonstaticdemo/SimpleNonStaticBlock.java
@@ -0,0 +1,20 @@
+package com.company.nonstaticdemo;
+
+/*
+* Non static block is load before constructor of class
+* it is specific to class object
+* Every time when new object is created it non static block is called
+*
+* */
+
+public class SimpleNonStaticBlock {
+
+    {
+        System.out.println("I am Non Static Block and Call Before Any Object Creation");
+    }
+    public SimpleNonStaticBlock()
+    {
+        System.out.println("I am in Default Contructor");
+    }
+
+}
